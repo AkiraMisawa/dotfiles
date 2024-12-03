466f0243c105b08e225ee57becf5d0cbc68776c3
AkiraMisawa <ccccccccandy@gmail.com>
2024-12-03 21:29:05 +0900

    update wezterm config to be activale resizable border

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,6 +6,7 @@ config = {
     automatically_reload_config = true,
     enable_tab_bar = false,
     window_close_confirmation = "NeverPrompt",
+    window_decoration = "RESIZE", -- disable the title bar but enable the resizable border
 }
 
 return config
