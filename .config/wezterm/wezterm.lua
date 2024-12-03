0fc7457c9de7bc711a805bdcaf00047547e16029
AkiraMisawa <ccccccccandy@gmail.com>
2024-12-03 21:28:22 +0900

    update wezterm config not to prompt confirmation message

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ local config = wezterm.config_builder()
 config = {
     automatically_reload_config = true,
     enable_tab_bar = false,
+    window_close_confirmation = "NeverPrompt",
 }
 
 return config
