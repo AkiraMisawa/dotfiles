6f54ec98c4058b1590160c0a350f5c040df3a640
AkiraMisawa <ccccccccandy@gmail.com>
2024-12-03 22:24:36 +0900

    update background

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,9 +8,19 @@ config = {
     window_close_confirmation = "NeverPrompt",
     window_decorations = "RESIZE", -- disable the title bar but enable the resizable border
     default_cursor_style = "BlinkingBar",
-    color_scheme = "Nord (Gogh)",
+    color_scheme = "Tokyo Night (Gogh)",
     font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
     font_size = 12.5,
+    background = {
+        {
+            source = {
+                Color = "#282c35",
+            },
+            width = "100%",
+            height = "100%",
+            opacity = 0.85,
+        }
+    }
 }
 
 return config
