babb46de30fa759c1a98782910d34ecb9e7dd1b7
EffectiveMsw <ccccccccandy@gmail.com>
2024-12-03 22:37:36 +0900

    Feature/wezterm (#2)
    * add git config
    
    * add wezterm config
    
    * update wezterm config to automatically reload config
    
    * update wezterm config to disable tab bar
    
    * update wezterm config not to prompt confirmation message
    
    * update wezterm config to be activale resizable border
    
    * use blinking bar cursor
    
    * change color scheme to use Nord (Gogh)
    
    * fix typo in window decorations
    
    * change font
    
    * add starship
    
    * update background
    
    * oepn wezterm with full screen

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,9 +8,25 @@ config = {
     window_close_confirmation = "NeverPrompt",
     window_decorations = "RESIZE", -- disable the title bar but enable the resizable border
     default_cursor_style = "BlinkingBar",
-    color_scheme = "Nord (Gogh)",
+    color_scheme = "Tokyo Night (Gogh)",
     font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
     font_size = 12.5,
+    background = {
+        {
+            source = {
+                Color = "#282c35",
+            },
+            width = "100%",
+            height = "100%",
+            opacity = 0.85,
+        }
+    }
 }
+
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():toggle_fullscreen()
+end)
 
 return config
