2667d8285126eca1b021842a8a78c6df03583797
AkiraMisawa <ccccccccandy@gmail.com>
2024-12-03 22:33:49 +0900

    oepn wezterm with full screen

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -23,4 +23,10 @@ config = {
     }
 }
 
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():toggle_fullscreen()
+end)
+
 return config
