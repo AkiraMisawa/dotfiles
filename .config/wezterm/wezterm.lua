ded66a97de5b7c79d9dce523ae973823e3398bc0
AkiraMisawa <ccccccccandy@gmail.com>
2024-12-04 09:38:20 +0900

    add color to active tab

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -34,4 +34,22 @@ wezterm.on("gui-startup", function(cmd)
     window:gui_window():toggle_fullscreen()
 end)
 
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+    local background = "#5c6d74"
+    local foreground = "#FFFFFF"
+
+    if tab.is_active then
+        background = "#ae8b2d"
+        foreground = "#FFFFFF"
+    end
+
+    local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+    return {
+        { Background = { Color = background } },
+        { Foreground = { Color = foreground } },
+        { Text = title },
+    }
+end)
+
 return config
